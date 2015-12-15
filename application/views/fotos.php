17ef96d236e60305a3a63649cac68750dde90ece
dani <dani@pimpampum.net>
2015-12-15 12:22:55 +0100

    controlador i view fotos

---
+++ application/views/fotos.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css" type="text/css" media="screen"/>
+</head>
+<body>
+
+<div id="container">
+	<h1>Prova de vista</h1>
+
+	<div id="body">
+
+<?php echo $num ?>
+
+</div>
+</div>
+
+</body>
+</html>
